ef0e18847b0f7a048ca4d7f1a6460d60605c1250
Suraj Rimal <surajrimal50@gmail.com>
2021-08-01 20:51:00 -0500

    admin side django

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -11,6 +11,10 @@ class Pet(models.Model):
     sex = models.CharField(max_length=1,choices=SEX_CHOICES, blank=True)
     submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name

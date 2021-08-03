f196b9d2eb65fe1758207717cfe1336022b2f44b
Suraj Rimal <surajrimal50@gmail.com>
2021-08-02 22:50:52 -0500

    css js html added

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Pet(models.Model):
     SEX_CHOICES = [('M','Male'),('F', 'Female')]
     name = models.CharField(max_length=100)
-    submittter = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
     species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30, blank=True)
     description = models.TextField()
